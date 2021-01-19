bbc8324ea38b3aef96c8cb580589339ee5b9789b
lyz52807 <59039910+lyz52807@users.noreply.github.com>
2021-01-20 02:57:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/openwrt-passwall/v2ray-plugin
+rm -rf ./package/openwrt-passwall/v2ray
+rm -rf ./feeds/packages/net/https-dns-proxy
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/diy/luci-app-vssr-plus
+rm -rf ./feeds/diy/vssr
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
